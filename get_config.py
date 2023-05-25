91e36905de8cc8e0de5212defa3cecee19e41a10
Ismail Turkyilmazoglu <44230222+ismailTrk@users.noreply.github.com>
2023-05-26 01:44:41 +0300

    Update get_config.py

--- get_config.py
+++ get_config.py
@@ -1,39 +1,40 @@
 from netmiko import ConnectHandler
 import threading
-import time 
+import time
 import logging
 ##########################GLOBAL VALUES##########################
 threads=[]
-output=[]
+
 ##########################PROC TIME CALC#########################
 start_time = time.time()
+
 ##########################GET NETMIKO LOG########################
-logging.basicConfig(filename='netmiko.log', level=logging.DEBUG)
+logging.basicConfig(filename='/tmp/netmiko.log', level=logging.DEBUG)
 logger = logging.getLogger("netmiko")
 
 ##########################WIDGET BAR#############################
-
-
+#will update
+#
 #################################################################
 
-def get_cisco_names(cisco_device):
+def get_cisco_config(cisco_device):
     try:
         conn=ConnectHandler(**cisco_device)
-        conn.enable() 
-        get_name=conn.find_prompt()[:-1]                
+        conn.enable()
+        time.sleep(0.2)
+        get_name=conn.find_prompt()[:-1]
     except Exception as e:
         print(f"\nerr_host: {cisco_device['host']} ")
     else:
-        get_conf=conn.send_command("sh run")         
+        get_conf=conn.send_command("sh run")
         #print(get_name,get_conf)
         conn.disconnect()
-    # bck/{get_name} --> your backup directory path
-    with open(f"bck/{get_name}.txt","w") as f :
+
+    with open(f"/tmp/{get_name}.txt","w") as f :
         f.write(get_conf)
 
-with open("switches.txt") as f:
-    device=f.read().splitlines()    
-
+with open("/tmp/switches.txt") as f:
+    device=f.read().splitlines()
 
 for ip in device:
     cisco_device={"host":ip,
@@ -43,11 +44,8 @@ for ip in device:
                   "secret":"admin",
                   "device_type":"cisco_ios"
                  }
-    th=threading.Thread(target=get_cisco_names,args=(cisco_device,))
+    th=threading.Thread(target=get_cisco_config,args=(cisco_device,))
     threads.append(th)
-for th in threads:
-    th.start() 
-for th in threads:
-    th.join()
-stop_time = time.time()
-print(stop_time-start_time)
+[th.start() for th in threads]
+[th.join() for th in threads]
+print(time.time()-start_time)
